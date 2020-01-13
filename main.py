43fbe1e8376bcbb43ac1289a4037d3647bad7746
p3miki <p3miki@outlook.com>
2020-01-13 11:06:45 -0500

    Added new classes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from pygame import gfxdraw
 
+from game import player
+
 WHITE=(255,255,255)
 BLACK=(0,0,0)
 RED=(255,0,0)
@@ -8,34 +10,62 @@ GREEN=(0,255,0)
 BLUE=(0,0,255)
 FPS = 30
 
-def init():
-  pygame.init()
-  fpsClock = pygame.time.Clock()
+class Main:
+  def __init__(self):
+    pygame.init()
+    fpsClock = pygame.time.Clock()
 
-  screen = pygame.display.set_mode((400, 300), 0, 32)
-  pygame.display.set_caption("Tutorial")
-  execute(screen, fpsClock)
+    screen = pygame.display.set_mode((800, 600), 0, 32)
+    pygame.display.set_caption("Tutorial")
 
-def execute(screen, clock):
-  direction = "right"
-  first = [False, False, False, False]
-  posX = 20
-  posY = 20
+    self.first = player.Player(screen, (20, 20))
+    self.first.direction = [0,0]
 
-  screen.fill(WHITE) 
+    self.execute(screen, fpsClock)
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit(0)
-      if event.type == pygame.KEYDOWN:
-        print(event)
-      if event.type == pygame.KEYUP:
-        print(event)
+  def execute(self, screen, clock):
+    while True:
+      screen.fill(WHITE)
+      
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit(0)
+        if event.type == pygame.KEYDOWN:
+          try:
+            self.first.direction = self.mapKey(event.key)
+          except Exception:
+            continue
+        if event.type == pygame.KEYUP:
+          try:
+            self.first.direction = self.mapKeyRelease(event.key)
+          except Exception:
+            continue
+
+      # up -> 273
+      # down -> 274
+      # left -> 276
+      # right -> 275
+      self.first.move(self.first.direction, 2)
+      self.first.draw()
+      pygame.display.update()
+      clock.tick(FPS)
     
-    pygame.display.update()
-    clock.tick(FPS)
+  def mapKey(self, key):
+    return {
+      273: [self.first.direction[0], -1],
+      274: [self.first.direction[0], 1],
+      275: [1, self.first.direction[1]],
+      276: [-1, self.first.direction[1]]
+    }[key]
+  
+  def mapKeyRelease(self, key):
+    return {
+      273: [self.first.direction[0], 0],
+      274: [self.first.direction[0], 0],
+      275: [0, self.first.direction[1]],
+      276: [0, self.first.direction[1]]
+    }[key]
 
 if __name__ == "__main__":
-  init()
+  Main()
